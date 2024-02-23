3cec566e6680335ee55b48f5371491a43f1a10e7
alexisaguado <alexis.garagu@educa.jcyl.es>
2024-02-23 16:29:34 +0100

    first commit of the calculator without methods, only main

---
+++ calculadoraJaviAlexis/src/calculadorajavialexis/CalculadoraJaviAlexis.java
@@ -0,0 +1,45 @@
+
+package calculadorajavialexis;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexis García Aguado
+ * @author Javier Cuadrado García
+ */
+public class CalculadoraJaviAlexis {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+
+        while (!salir) {
+            mostrarMenu();
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    sumar();
+                    break;
+                case 2:
+                    restar();
+                    break;
+                case 3:
+                    multiplicar();
+                    break;
+                case 4:
+                    dividir();
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtelo de nuevo.");
+            }
+        }
+
+        sc.close();
+    }
+}
